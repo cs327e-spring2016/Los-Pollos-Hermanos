a7e0b1aaf2e02be058f4b8c6ae96a83f128002fa
juanfhinojosa <juanfhinojosa@gmail.com>
2016-04-24 21:46:38 -0500

    added custom query(beta 1)
    first custom query interface integration

--- heisenberg.py
+++ heisenberg.py
@@ -4,6 +4,9 @@ import pymysql
 
 def main():
 
+	print("Starting to scrape...")
+	print('...')
+
 	'''
 	--------- -------- ------- CLEAR TABLES BEFORE RUNNING ------- - ------- --------- 
 	'''
@@ -12,19 +15,11 @@ def main():
 
 	cur = conn.cursor()
 
+	'''
 	cur.execute("DELETE FROM Data")
 	cur.execute("DELETE FROM Player")
 	cur.execute("DELETE FROM Opponents")
 	cur.execute("DELETE FROM Games")
-
-	#print()
-
-	'''
-
-	< --------------------- Jimmy Butler --------------------------------------------------------------- >
-	< -------------------------------------------------------------------------------------------------- > 
-	
-	'''
 
 
 	player_array = ["jimmy-butler", "cameron-bairstow", "aaron-brooks", "mike-dunleavy", "cristiano-felicio", "pau-gasol", "taj-gibson", "justin-holidy", "doug-mcdermott", "nikola-mirotic", "e'twaun-moore", "joakim-noah", "bobby-ports", "derrick-rose", "tony-snell"]
@@ -32,6 +27,7 @@ def main():
 	id_num = 0
 	game_id = 0
 	oppo_bool = False
+
 
 	for plyer in range(1,(len(player_array)+1)):
 
@@ -61,15 +57,10 @@ def main():
 		cur.execute('insert into Player values ("%s","%s","%s","%s")' % (id_num,first,last,age))
 
 
-
-
-		'''
-
-
-		'''
 		#< ------------- vs --------------------------------------------------- >
 		
 		#game_id = 0
+
 		
 		for v in range(1,30):
 
@@ -228,6 +219,80 @@ def main():
 			#oppo_bool = False
 
 		print("Player: ",player_array[plyer-1],"is done!")
+
+	'''
+
+	start = input("Ready to start querying?(yes/no) ")
+
+	if start.lower() != "yes":
+		print("BYE!")
+		return
+
+	print("GREAT!")
+	print()
+
+	c = True
+
+	while c == True:
+		print("Tables: Player, Data, Games")
+		print()
+		table = input("What table would you like to query? ")
+		print("Thanks!")
+		print()
+
+		if (table.lower() == "player"):
+			print("Table", table.capitalize(), "has: id_pk , fist , last ,and age for each player")
+			print()
+			select = input("What would you like to select(use commas to separate values)? ")
+			select2 = select.split(",")
+			print(select)
+			c = False
+		elif (table.lower() == "data"):
+			print("Table", table.capitalize(), "has: id_pk , game_fk , player_fk , minutes  , fg_made , fg_attempted , three_made , three_attempted , free_made , free_attempted , rebounds , assists , blocks , steals , fouls , turnovers ,  points for each Player in each Game played")
+			print()
+			select = input("What would you like to select(use commas to separate values)? ")
+			select2 = select.split(",")
+			print(select)
+			c = False
+		elif (table.lower() == "games"):
+			print("Table", table.capitalize(), "has: id_pk , opponent , date , and score for each game")
+			print()
+			select = input("What would you like to select(use commas to separate values)? ")
+			select2 = select.split(",")
+			print(select)
+			c = False
+		else:
+			print("Seems like your spelling may be incorrect, lets try again.")
+			c = True
+
+	print("PASSED")
+	'''
+	y = "concat(first, ' ' ,last)"
+	hfg = cur.execute('select %s from Player' % y)
+	p = cur.fetchall()
+	for row in p:
+		print(row)
+	'''
+
+	print("What conditions would you like to add(ex first = jimmy, age >= 20)?")
+	print()
+	whre = input("WHERE: ")
+
+	print(whre)
+
+	print("PASSED")
+
+	print("Is this the query you'd like to run?")
+	print()
+	table = table.capitalize()
+	print("SELECT", select, "FROM",table, "WHERE", whre)
+	statement = input("yes/no : ")
+	if (statement.lower() == "yes"):
+		hfg = cur.execute('SELECT %s FROM %s WHERE %s' % (select, table, whre))
+		p = cur.fetchall()
+		for row in p:
+			print(row)
+
 
 	cur.close()
 	conn.commit()
