b8af0350ab1d22819f427097ee91061fb7a01510
juanfhinojosa <juanfhinojosa@gmail.com>
2016-04-25 08:35:46 -0500

    added comments to ALPHA

--- heisenberg.py
+++ heisenberg.py
@@ -7,27 +7,25 @@ def main():
 	print("Starting to scrape...")
 	print('...')
 
-	'''
-	--------- -------- ------- CLEAR TABLES BEFORE RUNNING ------- - ------- --------- 
-	'''
 
+	# database connection
 	conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='jfh71293.,', db='data_scraper')
-
 	cur = conn.cursor()
 
+	# tables wiped 
 	cur.execute("DELETE FROM Data")
 	cur.execute("DELETE FROM Player")
 	cur.execute("DELETE FROM Opponents")
 	cur.execute("DELETE FROM Games")
 
-
+	# chicago bulls roster
 	player_array = ["jimmy-butler", "cameron-bairstow", "aaron-brooks", "mike-dunleavy", "cristiano-felicio", "pau-gasol", "taj-gibson", "justin-holidy", "doug-mcdermott", "nikola-mirotic", "e'twaun-moore", "joakim-noah", "bobby-ports", "derrick-rose", "tony-snell"]
 
 	id_num = 0
 	game_id = 0
 	oppo_bool = False
 
-
+	# iterate over roster
 	for plyer in range(1,(len(player_array)+1)):
 
 		ayer = player_array[plyer-1]
@@ -37,6 +35,7 @@ def main():
 
 		player = player_array[plyer-1]
 
+		# scrape begins
 		html = urlopen("http://espn.go.com/nba/player/gamelog/_/id/6430/"+player)
 		bsObj = BeautifulSoup(html.read(), "html.parser")
 
@@ -57,9 +56,9 @@ def main():
 
 		cur.execute('insert into Player values ("%s","%s","%s","%s")' % (id_num,first,last,age))		
 
-		
+		# iterate over nba teams
 		for v in range(1,30):
-
+			# all but chicago bulls
 			if (v == 4):
 				continue
 			else:
@@ -112,12 +111,13 @@ def main():
 
 					points = obj[16].getText()
 
+					# only need one data instance of each game
 					if plyer == 1:
 
 						cur.execute('insert into Games values ("%s","%s","%s","%s")' % (game_id, opponent , date, score))
-
+					# data injection per player and game
 					cur.execute('insert into Data values ("%s","%s","%s","%s", "%s", "%s","%s","%s","%s", "%s" ,"%s", "%s", "%s","%s","%s","%s", "%s")' % (game_id, game_id ,id_num, minu, fg_made, fg_attempted, three_made, three_attempted, free_made, free_attempted, rebounds, assists, blocks, steals, fouls, turnovers, points ))
-
+				# same as above, separation due to html structure
 				for i in range(len(st2)):
 
 					if plyer == 1:
@@ -169,8 +169,9 @@ def main():
 						cur.execute('insert into Games values ("%s","%s","%s","%s")' % (game_id, opponent , date, score))
 
 		print("Player: ", ayer[0].capitalize(), ayer[1].capitalize(),"is done!")
-
+		# end of scrape
 	print()
+	# custom query interface
 	start = input("Ready to start querying?(yes/no) ")
 
 	if start.lower() != "yes":
@@ -181,7 +182,7 @@ def main():
 	print()
 
 	c = True
-
+	# iterate until correct table name is specified
 	while c == True:
 		print("Tables: Player, Data, Games")
 		print()
@@ -192,6 +193,7 @@ def main():
 		if (table.lower() == "player"):
 			print("Table", table.capitalize(), "has: id_pk , fist , last ,and age for each player")
 			print()
+			# user inputs what columns they'd like to see
 			select = input("What would you like to select(use commas to separate values)? ")
 			select2 = select.split(",")
 			print(select)
@@ -214,13 +216,14 @@ def main():
 			print("Seems like your spelling may be incorrect, lets try again.")
 			c = True
 
-
+	# user input WHERE clause
 	print("What conditions would you like to add(ex first = 'jimmy', age >= 20)?")
 	print()
 	whre = input("WHERE: ")
 
 	print()
 
+	# user validation of custom query
 	print("Is this the query you'd like to run?")
 	print()
 	table = table.capitalize()
@@ -228,13 +231,14 @@ def main():
 	print()
 	statement = input("(yes/no): ")
 	print()
+	# sql query construction and output
 	if (statement.lower() == "yes"):
 		hfg = cur.execute('SELECT %s FROM %s WHERE %s' % (select, table, whre))
 		p = cur.fetchall()
 		for row in p:
 			print(row)
 
-
+	# close database connection
 	cur.close()
 	conn.commit()
 	conn.close()
